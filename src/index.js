fef5c8bc50577d6da01e56e9bb4c40a8109c03a9
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-10-19 02:58:29 +0700

    3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const greeting = {
-  morning: "Good morning"
+const date = new Date(2021, 5, 5, 8);
+
+const timeSel = {
+  word: String
 };
 
-ReactDom.render(<h1>Good morning</h1>, document.getElementById("root"));
+const colSel = {
+  color: String
+};
+
+if (date.getHours() >= 0 && date.getHours() <= 12) {
+  timeSel.word = "Good Morning.";
+  colSel.color = "red";
+} else if (date.getHours() > 12 && date.getHours() <= 18) {
+  timeSel.word = "Good Afternoon.";
+  colSel.color = "green";
+} else {
+  timeSel.word = "Good Evening.";
+  colSel.color = "blue";
+}
+
+ReactDom.render(
+  <h1 style={colSel} className="heading">
+    {timeSel.word}
+  </h1>,
+  document.getElementById("root")
+);
 
 //Create a React app from scratch.
 //Show a single h1 that says "Good morning" if between midnight and 12PM.
