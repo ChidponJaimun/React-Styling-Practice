0935aed08fdec83f3ebcb9578554f06f933543f8
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2023-07-24 03:08:54 +0700

    cc cc

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@
 //Apply the "heading" style in the styles.css
 //Dynamically change the color of the h1 using inline css styles.
 //Morning = red, Afternoon = green, Night = blue.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <p style={customStyle}>Created by {fName + " " + lName}.</p>
+    <p>Copyright {year}.</p>
+  </div>,
+  document.getElementById("root")
+);
